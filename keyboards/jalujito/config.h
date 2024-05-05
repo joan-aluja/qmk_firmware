325e0bde67c3d456125e9fa3fbf2c9dc346d56bb
Joan Aluja Oraa <joan.aluja@estudiantat.upc.edu>
2024-05-05 13:07:40 +0200

    jalujito keyboard

---
+++ keyboards/jalujito/config.h
@@ -0,0 +1,22 @@
+// Copyright 2024 Joan Aluja Oraá (@joan-aluja)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define SPLIT_HAND_PIN GP23
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
---
+++ keyboards/jalujito/rules.mk
@@ -0,0 +1,3 @@
+# Left blank
+SERIAL_DRIVER = vendor
+
